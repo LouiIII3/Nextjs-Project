a5fe4fb6301a40deadfeabda3cced17831487db3
LouiIII3 <jikdsunghuilee@naver.com>
2024-07-30 15:27:40 +0900

    드디어

---
+++ app/ui/font.ts
@@ -0,0 +1,14 @@
+// import {Inter, Lusitana} from 'next/font/google';
+
+// //외부에서 import 하여 사용 할수 있도록 작성
+// export const inter = Inter({ subsets: ['latin']});
+
+
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+ 
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
